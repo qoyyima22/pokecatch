9ab3e1adc3892cec47754f739b55ab17e068c3d0
Qoyyima Salam <qoyyima@adminlocal.klob.id>
2021-05-03 21:19:51 +0700

    update webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,14 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
-    })
+    }),
+    new CopyWebpackPlugin([{ from: 'public' }])
   ]
 });
